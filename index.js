1a431a51bcd1dc33e34ad567594b3f95a80b312c
Tomer du Sautoy <tomerdusautoy@gmail.com>
2019-07-10 11:33:56 -0500

    heroku deployment

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+require('dotenv').config()
 const server = require('./api/server.js');
+
+const port = process.env.PORT || 5000;
 
 server.listen(4000, () => {
   console.log('\n*** Server Running on http://localhost:4000 ***\n');
