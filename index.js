b13b52fe8a5617d63d364aca167181fc458b3f95
Tomer du Sautoy <tomerdusautoy@gmail.com>
2019-07-10 11:38:38 -0500

    add

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const server = require('./api/server.js');
 
 const port = process.env.PORT || 5000;
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
